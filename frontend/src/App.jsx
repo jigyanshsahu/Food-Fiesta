8793e85edb3655cf379d727c9bafb336283aa764
Jigyansh sahu <indianinja008@gmail.com>
2026-04-21 22:17:39 +0530

    H

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home/Home.jsx";
+import Home from "./pages/Home/Home.jsx"
 
 import Cart from './pages/Cart/Cart';
 import Placeorder from './pages/Placeorder/Placeorder.jsx';
